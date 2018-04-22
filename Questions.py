d1497a70454db64536675967d9214ab958b68cb2
nguan <nguan@smith.edu>
2018-04-22 15:29:15 -0400

    Nancy's update

--- Questions.py
+++ Questions.py
@@ -1,5 +1,5 @@
 #import pygame as pgimport random
-import time
+import time,random
 from graphics import *
 from os import listdir
 from os.path import isfile, join
@@ -7,10 +7,9 @@ from os.path import isfile, join
 """Bugs to fix:
         up & down有点卡
         need to incorporate option box object
-
 """
 def test():
-    win = GraphWin("Questions", 600, 600)
+    win = GraphWin("Questions", 500, 500)
     g1 = Questions(win)
     if g1.display():
         pass #accelerate
@@ -25,8 +24,8 @@ class Questions():
     #set up display and load text
     def __init__(self,win):
         self.win = win
-        #f=randomQ("pathway to directory containing text")
-        f=open("question1.txt")
+        f=self.randomQ("CSC220-Final/conversation box text/questions/")
+        #f=open("question1.txt")
         lines = [ line for line in f]
         self.question = lines[0]
         self.options = lines[1:5]
@@ -38,8 +37,8 @@ class Questions():
     
     #input directory containging all the files, return a file containg the question
     def randomQ(self,mypath):
-        files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-        idx=random.randint(1,len(files))
+        files = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
+        idx=random.randint(0,len(files)-1)
         f = open(files[idx],'r')
         return f
 
@@ -56,7 +55,7 @@ class Questions():
         return (int)(self.maxtime -(time - starttime))
 
     def timeisup(self,starttime,time):
-        if time-starttime>10:
+        if time-starttime>self.maxtime:
             return True
         else:
             return False
@@ -67,27 +66,55 @@ class Questions():
         win = self.win
         win.setBackground("white")
         #qustionBox
-        self.questiontext = Text(Point(300,100),self.question)
-        self.questiontext.draw(win)
+        questionbox = Image(Point(250,100),"CSC220-Final/ui/conversation_box/conversation_box_resize.png")
+        #self.questionbox.draw(win)
+        questiontext = Text(Point(250,100),self.question)
+        questiontext.draw(win)
+        answerbox = Image(Point(250,250),"CSC220-Final/ui/conversation_box/conversation_box_resize.png")
+        answerbox.draw(win)
         #option box 
 
-        textA = Text(Point(300,200),self.options[0])
-        textA.draw(win)
-        textB = Text(Point(300,250),self.options[1])
-        textB.draw(win)
-        textC = Text(Point(300,300),self.options[2])
-        textC.draw(win)
-        textD = Text(Point(300,350),self.options[3])
-        textD.draw(win)
-        
-        marker =  0
+        self.optionlist = [Option(win,self.options[i],Point(250,200+50*i)) for i in range(4)]
+        for opt in self.optionlist:
+            opt.draw()
 
-        timeDisplay = Text(Point(500,50),"0:00")
+        marker =  1
+
+        timeDisplay = Text(Point(450,50),"0:00")
         timeDisplay.draw(win)
         currenttime=self.maxtime
         starttime = time.time()
 
         while True:
+            
+            if win.checkKey()== 'Return': #<Enter>
+                break
+ 
+            if win.checkKey()=='Down':#keys.Down:
+                #deselect previous mark
+                self.optionlist[marker-1].isSelected=False
+                if marker == 4:
+                    marker =1
+                else:
+                    marker+=1
+                #redraw option box for marker, show it in different color
+                self.optionlist[marker-1].isSelected=True
+                for opt in self.optionlist:
+                    opt.undraw()
+                    opt.draw()
+
+            if win.checkKey()=='Up':#keys.Up:
+                self.optionlist[marker-1].isSelected=False
+                if marker <= 1:
+                    marker =4
+                else:
+                    marker-=1
+                #redraw option box for marker, show it in different color
+                self.optionlist[marker-1].isSelected=True
+                for opt in self.optionlist:
+                    opt.undraw()
+                    opt.draw()
+
             if currenttime != self.getRemainingTime(starttime,time.time()):
                 self.displayTime(starttime,time.time(),timeDisplay)
                 timeDisplay.undraw()
@@ -98,43 +125,69 @@ class Questions():
                 timeDisplay.draw(win)
                 break
 
-            if win.checkKey()== 'Return': #<Enter>
-                break
- 
-            if win.checkKey()=='Down':#keys.Down:
-                if marker == 4:
-                    marker =1
-                else:
-                    marker+=1
-                #redraw option box for marker, show it in different color
-            if win.checkKey()=='Up':#keys.Up:
-                if marker <= 1:
-                    marker =4
-                else:
-                    marker-=1
-                #redraw option box for marker, show it in different color
-
+        #if correct
         if str(marker) == self.answer:
-            msg = Text(Point(300,500),"Correct! You get the acceleration bonus!")
+            msg = Text(Point(250,450),"Correct! You get the acceleration bonus!")
             result = False
+            for opt in self.optionlist:
+                opt.undraw()
+            answerbox.undraw()
+            answerbox = Image(Point(250,250),"CSC220-Final/ui/conversation_box/right.png")
+            answerbox.draw(win)
+            for opt in self.optionlist:
+                opt.draw()
+        #if incorrect
         else:
-            msg = Text(Point(300,500),"Wrong! You will be decelerated!")
+            msg = Text(Point(250,450),"Wrong! You will be decelerated!")
             result = True
-            
+            for opt in self.optionlist:
+                opt.undraw()
+            answerbox.undraw()
+            answerbox = Image(Point(250,250),"CSC220-Final/ui/conversation_box/wrong.png")
+            answerbox.draw(win)
+            for opt in self.optionlist:
+                opt.draw()
+            self.optionlist[int(self.answer)-1].undraw()
+            self.optionlist[int(self.answer)-1].drawWrong()
         msg.draw(win)
-        time.sleep(1)
+        time.sleep(3)
         msg.undraw()
         timeDisplay.undraw()
-        textA.undraw()
-        textB.undraw()
-        textC.undraw()
-        textD.undraw()
-        self.questiontext.undraw()
+        for opt in self.optionlist:
+            opt.undraw()
+        questiontext.undraw()
+        answerbox.undraw()
+
         return result
     
 
 
+class Option():
+    def __init__(self,win,text,position):
+        self.win=win
+        self.text=Text(position,text)
+        self.text.setSize(10)
+        self.isSelected=False
+        self.position=position
+        #self.optionbox
 
+
+    def draw(self):
+        if self.isSelected:
+            self.optionbox = Image(self.position,"CSC220-Final/ui/conversation_box/button_right_resize.png")
+        else:
+            self.optionbox = Image(self.position,"CSC220-Final/ui/conversation_box/button_normal_resize.png")
+        self.optionbox.draw(self.win)
+        self.text.draw(self.win)
+
+    def drawWrong(self):
+        self.optionbox = Image(self.position,"CSC220-Final/ui/conversation_box/button_wrong.png")
+        self.optionbox.draw(self.win)
+        self.text.draw(self.win)
+
+    def undraw(self):
+        self.optionbox.undraw()
+        self.text.undraw()
 
 
 test()
