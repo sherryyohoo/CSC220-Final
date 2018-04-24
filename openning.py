f1d58fcf1fb639b7228dd5cd326adf0bf5c3c097
WindyLilyQian <lqian@smith.edu>
2018-04-23 21:09:07 -0400

    This is file Lily need help

---
+++ openning.py
@@ -0,0 +1,29 @@
+from graphics import *
+from random import *
+from math import sqrt
+import time
+
+def Openscene(win,x,y,open1,open2,open3):
+        open1_image = Image(Point(x, y),open1)
+        open1_image.draw(win)
+        time.sleep(100)
+        open1_image.undraw()
+        open2_image = Image(Point(x, y),open2)
+        open2_image.draw(win)
+        time.sleep(80)
+        open2_image.undraw()
+        open3_image = Image(Point(x, y),open3)
+        open3_image.draw(win)
+        time.sleep(50)
+       # open3_image.undraw()
+        
+def main():
+    win = GraphWin( 'openning', 800, 500, autoflush=False )
+    win.setBackground( 'white' )
+    w = 100
+    win.setCoords( -w, -w, w, w )
+    Openscene(win,0,0,"open1.gif","open2.gif","open3.gif")
+
+main()
+
+    
