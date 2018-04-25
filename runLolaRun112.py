46ca83ebd7fcf1647396f0de82fac44c62a0e761
WindyLilyQian <lqian@smith.edu>
2018-04-25 16:18:47 -0400

    comments

--- runLolaRun112.py
+++ runLolaRun112.py
@@ -1,3 +1,8 @@
+##CSC 220 Final Project
+##Instructor: Jordan Crouser
+##Group Members: Nancy Guan;Lucy Yilin Wang, Siyu Lily Qian, Sherry Zhenyao Cai
+
+#import relevant modules 
 from graphics import *
 from random import *
 from math import sqrt
@@ -5,68 +10,129 @@ from Questions import *
 from TypeGame import *
 import time
 import itertools
-import threading
 import sys
 
+
+#------------------
+# HELPER FUNCTION
+#------------------
+
 def Openscene(win,x,y,open1,open2,open3):
+        '''Helper function of the openning of the game, to introduce game background
+        and instructions through showing openning images in sequence
+        x,y: the position to draw the image 
+        open1,open2,open3: the file names of the images that will be displayed at the
+        begining 
+        window: the window on which the images draw'''
+        
+        #call openning images and draw the images
         open1_image = Image(Point(x, y),open1)
         open1_image.draw(win)
+        #update the system
         update()
-        time.sleep(5)
+        #freeze the system for 3 seconds 
+        time.sleep(3)
+        #undraw the first image, call and draw the second image 
         open1_image.undraw()
         open2_image = Image(Point(x, y),open2)
         open2_image.draw(win)
+        #update the system
         update()
-        time.sleep(15)
+        #freeze the system for 13 seconds
+        time.sleep(13)
+        #undraw the second image, call and draw the third image 
         open2_image.undraw()
         open3_image = Image(Point(x, y),open3)
         open3_image.draw(win)
+        #update the system
         update()
-        time.sleep(5)
+        #freeze the system for 3 seconds
+        time.sleep(3)
+        #undraw the third image
         open3_image.undraw()
+
+#---------
+# CLASSES
+#---------
+
         
 class Timer:
+    '''Constructor for the timer class, create a time bar to time the game,
+    make the timer move along the time bar to show how much time has passed/left
+    for this game
+    self: a place-holder for specific instances
+    x,y: the location to draw the bar and the timer
+    clock: filename of the timer image that will be draw
+    win: the window on which the images draw'''
     def __init__(self,x,y,clock,win):
+        #create a time bar 
         p1 = Point(x-100,y-5)
         p2 = Point(x+100,y+5)
         bar1 = Rectangle(p1,p2)
         bar1.setFill("light green")
         bar1.setOutline("light green")
+        #set the moving distance of the timer
+        #instance variables for speed 
         self.speed = 0.1
         #call image timer and draw the image:timer
-        clock_filename = clock 
-        clock_image = Image(Point(x-100, y),clock_filename )
+        clock_image = Image(Point(x-100, y),clock)
+        #instance variables for clock image
         self.clock_image = clock_image
+        #draw timer and time bar 
         bar1.draw(win)
         clock_image.draw(win)
-      
+
+    #move the timer  
     def move_to(self):
         self.clock_image.move(self.speed,0)
 
 class Progress:
-    def __init__(self,x,y,distance,lola,win):
+    '''Constructor for the Progress class, create a progress bar and make the icon
+    move along the bar to show how much journey lola has left for this game
+    self: a place-holder for specific instances
+    x,y: the location to draw the bar and the icon
+    distance: the total distance of Lola's journey (running path)
+    lola: filename of the icon image that will be draw
+    win: the window on which the images draw'''
+        
+    def __init__(self,x,y,distance,icon,win):
+        #instance variables for distance 
         self.distance = distance
+        #create and draw the progress bar 
         p1 = Point(x-100,y-5)
         p2 = Point(x+100,y+5)
         bar1 = Rectangle(p1,p2)
         bar1.setFill("light blue")
         bar1.setOutline("light blue")
         bar1.draw(win)
-        lola_image = Image(Point(x-100, y),lola)
-        self.lola_image = lola_image
-        lola_image.draw(win)
+        #call and draw the icon image 
+        icon_image = Image(Point(x-100, y),icon)
+        self.icon_image = icon_image
+        icon_image.draw(win)
 
     def getx(self):
-        image_position = self.lola_image.getAnchor()
+        #get the current position of the icon image 
+        image_position = self.icon_image.getAnchor()
         image_x = 100-abs(image_position.getX())
+        #store the value of the current position 
         return(image_x)
 
         
-    def move_to(self,x,progress):
-        lola_curr = (progress/self.distance)*200
-        speed =  lola_curr-x
+    def move_to(self,icon_x,progress):
+        #icon_x: icon's current x-axis position
+        #progress: Lola's progress in the game 
+        #calculate where the icon should currently be placed
+        #based on Lola's progress in the game over the total distance of the game
+        #(the current percentage of Lola's progress in the game)
+        #in proportion to the length of the bar 
+        icon_curr = (progress/self.distance)*200
+        #the length of movement (speed) is the icon's (should be) current position
+        #minus the origional icon's position 
+        speed =  icon_curr-icon_x
+        #round up the number 
         speed = round(speed,2)
-        self.lola_image.move(speed,0)
+        #move the icon through the progress bar
+        self.icon_image.move(speed,0)
       
 
 
@@ -235,14 +301,20 @@ class BgAndObj:
             newX, newY=x+dx, y+dy
             ObjCenter=Point(newX,newY)'''
 
+#------
+# MAIN
+#------
 def main():
+    #construct the width, height,and background color of the window
     win = GraphWin( 'Run, Lola, run', 800, 500, autoflush=False)
     win.setBackground( 'cornflower blue' )
+    #set up the coordinates of the window 
     w = 100
     win.setCoords( -w, -w, w, w )
-    #Openinng 
-    #Openscene(win,0,0,"open1.gif","open2.gif","open3.gif")
-    
+    #Call the Openscene function to begin the game with the openning scenes 
+    Openscene(win,0,0,"open1.gif","open2.gif","open3.gif")
+    #set up Lola's initial speed, background center,object radius, number of objects,
+    #background picture names
     initialSpeed=10
     BgCenter=Point(0,0)
     objRadius=5
@@ -264,9 +336,13 @@ def main():
     lola=Lola(win, center1, distance1,speed1,jumpHeight1,LolaRadius)
     t = 200 #this represent #*sleep time seconds
     totalDistance = 200 #To be determined
+    #Call the timer class to initialize the time bar 
     timer = Timer(0,90,"timer.png",win)
+    #Call Progress class to initialize the progress bar 
     progress = Progress(0,80,totalDistance,"german.png",win)
-    
+
+    #while loop to start the game
+    #make lola runs 
     while(t>0 and lola.getDistance()<totalDistance):
         down=False 
         h=-20
@@ -346,9 +422,12 @@ def main():
         time.sleep(0.1)
         gif4.undraw()'''
         t=t-1
+        #call the move_to method in Timer class to move the timer for each loop 
         timer.move_to()
-        x = progress.getx()
-        progress.move_to(x,distance1)
+        #track the icon's position 
+        icon_x = progress.getx()
+        #call the move_to method in Progress class to move the icon for each loop
+        progress.move_to(icon_x,distance1)
         #collision checker and enters game
         objectCenters=bgAndObj.getCentersForObjects()
         #print("objectYCenters: ", objectCenters)
